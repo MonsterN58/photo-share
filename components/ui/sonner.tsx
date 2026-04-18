c422afb2fe88daa47792e615a346b7db091a8395
G13 ShoQin24 <2721439803@qq.com>
2026-04-19 02:36:30 +0800

    npm run build修改

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -12,7 +12,6 @@ const Toaster = ({ ...props }: ToasterProps) => {
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
       closeButton
-      closeButtonAriaLabel="Close notification"
       icons={{
         success: (
           <CircleCheckIcon className="size-4" />
